2dc637c94b6dca5deed6908fafa007225a9b9925
leonidi86 <leonidi368@gmail.com>
2024-02-08 12:03:25 +0300

    menu

---
+++ Assets/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SlimUI.ModernMenu{
+    public class ExtraLinks : MonoBehaviour{
+        public void CCP(){
+            Application.OpenURL("https://vk.com/komynistid");
+        }
+
+        public void Clean1(){
+            Application.OpenURL("https://vk.com/evnrss");
+        }
+
+        public void Essence(){
+            Application.OpenURL("https://vk.com/leonidi86");
+        }
+    }
+}
